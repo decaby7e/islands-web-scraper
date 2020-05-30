45efa6962a074f81b606e076ac4d2e2fb30484f7
decaby7e <27081624+decaby7e@users.noreply.github.com>
2020-05-30 01:52:04 -0400

    🧹 Refactoring

--- main.py
+++ main.py
@@ -21,223 +21,226 @@ from selenium.common.exceptions import NoSuchElementException
 from settings import *
 
 
-#
-# Selenium initalization
-#
-
-
-print('INFO  Initalizing Selenium...')
-
-driver = webdriver.Firefox()
-driver.get("https://islands.smp.uq.edu.au")
-
-# Login
-driver.find_element_by_xpath("/html/body/div[3]/form/div/div[1]/input").send_keys(USERNAME)
-driver.find_element_by_xpath("/html/body/div[3]/form/div/div[2]/input").send_keys(PASSWORD)
-driver.find_element_by_xpath("/html/body/div[3]/form/div/div[3]/input").click()
-
-# Go to island
-driver.get(f"https://islands.smp.uq.edu.au/village.php?{VILLAGE}")
-
-
-#
 # Get all the residents
-#
+def get_resident_list(driver):
+    
+    print('INFO  Fetching residents...')
 
+    resident_list = []
 
-print('INFO  Fetching residents...')
+    for i in HOUSE_LIST:
 
-# DEBUG: Get all the residents from a JSON
-# resident_list = json.load(open('backup.json'))
+        driver.execute_script(f"getHouse({i - 1})")
+        time.sleep(1.4)
+        resident_table = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div/div/table[1]")
+        resident_rows = resident_table.find_elements_by_tag_name("tr")
 
-resident_list = []
+        for row in resident_rows:
+            name = row.find_element_by_class_name("resident").text
+            gender = row.find_element_by_class_name("gender").text
+            age = row.find_element_by_class_name("age").text
+            resident_link = row.find_element_by_tag_name("a")
+            resident_id = resident_link.get_attribute("href")[-10:]
 
-for i in HOUSE_LIST:
+            if gender == "♀":
+                gender = "female"
+            elif gender == "♂":
+                gender = "male"
 
-    driver.execute_script(f"getHouse({i - 1})")
-    time.sleep(1.4)
-    resident_table = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div/div/table[1]")
-    resident_rows = resident_table.find_elements_by_tag_name("tr")
-
-    for row in resident_rows:
-        name = row.find_element_by_class_name("resident").text
-        gender = row.find_element_by_class_name("gender").text
-        age = row.find_element_by_class_name("age").text
-        resident_link = row.find_element_by_tag_name("a")
-        resident_id = resident_link.get_attribute("href")[-10:]
-
-        if gender == "♀":
-            gender = "female"
-        elif gender == "♂":
-            gender = "male"
-
-        if gender == GENDER and (int(age) >= 20 and int(age) <= 79):
-            if VERBOSE:
-                print(f'DEBUG    Adding {name} ({gender}, {age}) from {i}')
-            resident_list.append(
-                {
-                    "name": name,
-                    "gender": gender,
-                    "age": age,
-                    "id": resident_id,
-                    "home_num": i,
-                    "tests": {},
-                }
-            )
-
-
-#
-# Ask for consent from residents
-#
-
-
-print('INFO  Obtaining consent from residents...')
-
-print('DEBUG  Not implemented!')
-
-
-#
-# Run tests (Javascript) on all residents
-#
-
-
-print('INFO  Running tests on residents...')
-
-test_list = [
-    "bloodpressure",
-    "temperature",
-    "peakflow",
-    "vocalfreq",
-    "memorycards",
-    "questionnaire"
-]
-
-for test in test_list:
-    for resident in resident_list:
-        if VERBOSE:
-            print(f'DEBUG    Testing {resident["name"]} w/ {test}')
-        # Go to the resident page
-        driver.get(f"https://islands.smp.uq.edu.au/islander.php?id={resident['id']}")
-
-        driver.execute_script(f'startTask("{test}")')
-
-    time.sleep(110)
-
-
-#
-# Collect test results
-#
-
-
-print('INFO  Extracting test results from website...')
-
-for resident in resident_list:
-
-    print(f' == {resident["name"]} ==')
-
-    # task_pane = driver.find_element_by_xpath('//*[@id="t2"]')
-    driver.get(f"https://islands.smp.uq.edu.au/islander.php?id={resident['id']}")
-    driver.execute_script("change_tab('t2')")
-    tests = driver.find_elements_by_class_name("taskresult")
-
-    print(f'DEBUG  {len(tests)} tests')
-
-    test_collection = {
-        "bp": '',
-        "temp": '',
-        "flow": '',
-        "vocal": '',
-        "memory": '',
-        "university": '',
-        "run": '',
-        "bike": '',
-        "swim": '',
-        "smoke": '',
-        "drink": '',
-        "chocolate": '',
-        "pizza": '',
-        "sleep": '',
-        "attractive": '',
-        "superpower": '',
-    }
-
-    for test in tests:
-
-        try:
-            test_type = test.find_element_by_class_name("taskresulttask").text
-        except NoSuchElementException:
-            if VERBOSE:
-                print('DEBUG  Skipping extra taskresult...')
-            continue
-
-        try:
-            print(f'INFO  Processing {test_type} ({test.find_element_by_class_name("taskresultresult").text})')
-        except Exception:
-            pass
-
-        if test_type == "Blood Pressure":
-            test_collection["bp"] = test.find_element_by_class_name("taskresultresult").text
-        elif test_type == "Body Temperature":
-            test_collection["temp"] = test.find_element_by_class_name("taskresultresult").text
-        elif test_type == "Peak Flow Meter":
-            test_collection["flow"] = test.find_element_by_class_name("taskresultresult").text
-        elif test_type == "Vocal Frequency":
-            test_collection["vocal"] = test.find_element_by_class_name("taskresultresult").text
-        elif test_type == "Memory Test Cards":
-            test_collection["memory"] = test.find_element_by_class_name("taskresultresult").text
-        elif test_type == "":
-            try:
-                survey_results = test.find_elements_by_class_name("taskresultresponse")
-
-                test_collection["university"] = survey_results[0].text
-                test_collection["run"] = survey_results[1].text
-                test_collection["bike"] = survey_results[2].text
-                test_collection["swim"] = survey_results[3].text
-                test_collection["smoke"] = survey_results[4].text
-                test_collection["drink"] = survey_results[5].text
-                test_collection["chocolate"] = survey_results[6].text
-                test_collection["pizza"] = survey_results[7].text
-                test_collection["sleep"] = survey_results[8].text
-                test_collection["attractive"] = survey_results[9].text
-                test_collection["superpower"] = survey_results[10].text
-            except Exception:
+            if gender == GENDER and (int(age) >= 20 and int(age) <= 79):
                 if VERBOSE:
-                    print('DEBUG  Skipping blank test section...')
-        else:
-            print("WARN  Found a test we didn't perform...")
+                    print(f'DEBUG    Adding {name} ({gender}, {age}) from {i}')
+                resident_list.append(
+                    {
+                        "name": name,
+                        "gender": gender,
+                        "age": age,
+                        "id": resident_id,
+                        "home_num": i,
+                        "tests": {},
+                    }
+                )
 
-    resident["tests"] = test_collection
+
+# Ask for consent from residents
+def get_consent(driver, resident_list):
+    print('INFO  Obtaining consent from residents...')
+
+    print('DEBUG  Not implemented!')
+
+
+# Run tests (w/ Javascript) on all residents
+def run_tests(driver, resident_list):
+
+    print('INFO  Running tests on residents...')
+
+    test_list = [
+        "bloodpressure",
+        "temperature",
+        "peakflow",
+        "vocalfreq",
+        "memorycards",
+        "questionnaire"
+    ]
+
+    for test in test_list:
+        for resident in resident_list:
+            if VERBOSE:
+                print(f'DEBUG    Testing {resident["name"]} w/ {test}')
+            # Go to the resident page
+            driver.get(f"https://islands.smp.uq.edu.au/islander.php?id={resident['id']}")
+
+            driver.execute_script(f'startTask("{test}")')
+
+        time.sleep(110)
+
+
+# Collect test results
+def collect_tests(driver, resident_list):
+
+    print('INFO  Extracting test results from website...')
+
+    for resident in resident_list:
+
+        print(f' == {resident["name"]} ==')
+
+        # task_pane = driver.find_element_by_xpath('//*[@id="t2"]')
+        driver.get(f"https://islands.smp.uq.edu.au/islander.php?id={resident['id']}")
+        driver.execute_script("change_tab('t2')")
+        tests = driver.find_elements_by_class_name("taskresult")
+
+        print(f'DEBUG  {len(tests)} tests')
+
+        test_collection = {
+            "bp": '',
+            "temp": '',
+            "flow": '',
+            "vocal": '',
+            "memory": '',
+            "university": '',
+            "run": '',
+            "bike": '',
+            "swim": '',
+            "smoke": '',
+            "drink": '',
+            "chocolate": '',
+            "pizza": '',
+            "sleep": '',
+            "attractive": '',
+            "superpower": '',
+        }
+
+        for test in tests:
+
+            try:
+                test_type = test.find_element_by_class_name("taskresulttask").text
+            except NoSuchElementException:
+                if VERBOSE:
+                    print('DEBUG  Skipping extra taskresult...')
+                continue
+
+            try:
+                print(f'INFO  Processing {test_type} ({test.find_element_by_class_name("taskresultresult").text})')
+            except Exception:
+                pass
+
+            if test_type == "Blood Pressure":
+                test_collection["bp"] = test.find_element_by_class_name("taskresultresult").text
+            elif test_type == "Body Temperature":
+                test_collection["temp"] = test.find_element_by_class_name("taskresultresult").text
+            elif test_type == "Peak Flow Meter":
+                test_collection["flow"] = test.find_element_by_class_name("taskresultresult").text
+            elif test_type == "Vocal Frequency":
+                test_collection["vocal"] = test.find_element_by_class_name("taskresultresult").text
+            elif test_type == "Memory Test Cards":
+                test_collection["memory"] = test.find_element_by_class_name("taskresultresult").text
+            elif test_type == "":
+                try:
+                    survey_results = test.find_elements_by_class_name("taskresultresponse")
+
+                    test_collection["university"] = survey_results[0].text
+                    test_collection["run"] = survey_results[1].text
+                    test_collection["bike"] = survey_results[2].text
+                    test_collection["swim"] = survey_results[3].text
+                    test_collection["smoke"] = survey_results[4].text
+                    test_collection["drink"] = survey_results[5].text
+                    test_collection["chocolate"] = survey_results[6].text
+                    test_collection["pizza"] = survey_results[7].text
+                    test_collection["sleep"] = survey_results[8].text
+                    test_collection["attractive"] = survey_results[9].text
+                    test_collection["superpower"] = survey_results[10].text
+                except Exception:
+                    if VERBOSE:
+                        print('DEBUG  Skipping blank test section...')
+            else:
+                print("WARN  Found a test we didn't perform...")
+
+        resident["tests"] = test_collection
+
 
 # Export information to CSV / JSON
+def export(resident_list):
 
-print('INFO  Exporting to JSON...')
-with open('export.json', 'w') as export:
-    json.dump(resident_list, export)
+    print('INFO  Exporting to JSON...')
+    with open('export.json', 'w') as export:
+        json.dump(resident_list, export)
 
-print('INFO  Exporting to CSV...')
-with open('export.csv', 'w') as export:
-    for resident in resident_list:
-        line = (f"{resident['home_num']}|"
-            f"{resident['name']}|"
-            f"{resident['gender']}|"
-            f"{resident['age']}|"
-            f"{resident['tests']['bp']}|"
-            f"{resident['tests']['temp']}|"
-            f"{resident['tests']['flow']}|"
-            f"{resident['tests']['vocal']}|"
-            f"{resident['tests']['memory']}|"
-            f"{resident['tests']['university']}|"
-            f"{resident['tests']['run']}|"
-            f"{resident['tests']['bike']}|"
-            f"{resident['tests']['swim']}|"
-            f"{resident['tests']['smoke']}|"
-            f"{resident['tests']['drink']}|"
-            f"{resident['tests']['chocolate']}|"
-            f"{resident['tests']['pizza']}|"
-            f"{resident['tests']['sleep']}|"
-            f"{resident['tests']['attractive']}|"
-            f"{resident['tests']['superpower']}|")
-        export.write(line + '\n')
+    print('INFO  Exporting to CSV...')
+    with open('export.csv', 'w') as export:
+        for resident in resident_list:
+            line = (f"{resident['home_num']}|"
+                f"{resident['name']}|"
+                f"{resident['gender']}|"
+                f"{resident['age']}|"
+                f"{resident['tests']['bp']}|"
+                f"{resident['tests']['temp']}|"
+                f"{resident['tests']['flow']}|"
+                f"{resident['tests']['vocal']}|"
+                f"{resident['tests']['memory']}|"
+                f"{resident['tests']['university']}|"
+                f"{resident['tests']['run']}|"
+                f"{resident['tests']['bike']}|"
+                f"{resident['tests']['swim']}|"
+                f"{resident['tests']['smoke']}|"
+                f"{resident['tests']['drink']}|"
+                f"{resident['tests']['chocolate']}|"
+                f"{resident['tests']['pizza']}|"
+                f"{resident['tests']['sleep']}|"
+                f"{resident['tests']['attractive']}|"
+                f"{resident['tests']['superpower']}|")
+            export.write(line + '\n')
 
 
-driver.close()
+if __name__ == '__main__':
+    
+    #
+    # Selenium initalization
+    #
+
+    print('INFO  Initalizing Selenium...')
+
+    # Create browser
+    driver = webdriver.Firefox()
+    driver.get("https://islands.smp.uq.edu.au")
+
+    # Login
+    driver.find_element_by_xpath("/html/body/div[3]/form/div/div[1]/input").send_keys(USERNAME)
+    driver.find_element_by_xpath("/html/body/div[3]/form/div/div[2]/input").send_keys(PASSWORD)
+    driver.find_element_by_xpath("/html/body/div[3]/form/div/div[3]/input").click()
+
+    # Go to island
+    driver.get(f"https://islands.smp.uq.edu.au/village.php?{VILLAGE}")
+
+    #
+    # Procedures
+    #
+
+    # resident_list = json.load(open('backup.json'))  # DEBUG: Get all the residents from a JSON
+    resident_list = get_resident_list(driver)
+
+    get_consent(resident_list)
+    run_tests(resident_list)
+    collect_tests(resident_list)
+    export(resident_list)
+
+    driver.close()
--- settings.py
+++ settings.py
@@ -1,5 +1,9 @@
 import json
 
+##################################################################
+# General Configuration
+##################################################################
+
 with open("secrets.json") as secrets:
     secrets_json = json.load(secrets)
     USERNAME = secrets_json["username"]
@@ -7,59 +11,26 @@ with open("secrets.json") as secrets:
 
 VERBOSE = True
 
-# NOTE: These constants need to be set depending on what your desired
-#       sample is to be. (e.g. only males between 20 and 49)
+##################################################################
+# Villager Constraints
+#
+# These constants need to be set depending on what your desired
+# sample is to be. (e.g. only males between 20 and 49)
+##################################################################
+
 VILLAGE = 'Gordes'
+
 HOUSE_LIST = [
-    1,
-    2,
-    3,
-    11,
-    19,
-    26,
-    27,
-    30,
-    49,
-    51,
-    69,
-    70,
-    74,
-    83,
-    91,
-    100,
-    101,
-    104,
-    106,
-    112,
-    113,
-    118,
-    122,
-    125,
-    148,
-    150,
-    152,
-    158,
-    162,
-    177,
-    179,
-    192,
-    205,
-    206,
-    219,
-    223,
-    225,
-    234,
-    236,
-    240,
-    245,
-    246,
-    249,
-    251,
-    253,
-    257,
-    268,
-    269,
-    271,
-    275,
+    1,2,3,11,19,
+    26,27,30,49,51,
+    69,70,74,83,91,
+    100,101,104,106,112,
+    113,118,122,125,148,
+    150,152,158,162,177,
+    179,192,205,206,219,
+    223,225,234,236,240,
+    245,246,249,251,253,
+    257,268,269,271,275,
 ]
+
 GENDER = 'male'
