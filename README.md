b44d11b5d29c98968cb075adc14f45de9f4ced8d
decaby7e <27081624+decaby7e@users.noreply.github.com>
2020-05-30 01:36:06 -0400

    🎆 Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# Islands Web Scraper
+
+[See the site](https://islands.smp.uq.edu.au)
+
+## Installation
+
+First, install the system requirements:
+
+```bash
+$ apt install firefox-geckodriver
+```
+
+Then, install the Python requirements:
+
+```bash
+$ pip3 install -r requirements.txt
+```
+
+## To Do
+
+- [ ] Add useful logging messages
---
+++ settings.py
@@ -0,0 +1,65 @@
+import json
+
+with open("secrets.json") as secrets:
+    secrets_json = json.load(secrets)
+    USERNAME = secrets_json["username"]
+    PASSWORD = secrets_json["password"]
+
+VERBOSE = True
+
+# NOTE: These constants need to be set depending on what your desired
+#       sample is to be. (e.g. only males between 20 and 49)
+VILLAGE = 'Gordes'
+HOUSE_LIST = [
+    1,
+    2,
+    3,
+    11,
+    19,
+    26,
+    27,
+    30,
+    49,
+    51,
+    69,
+    70,
+    74,
+    83,
+    91,
+    100,
+    101,
+    104,
+    106,
+    112,
+    113,
+    118,
+    122,
+    125,
+    148,
+    150,
+    152,
+    158,
+    162,
+    177,
+    179,
+    192,
+    205,
+    206,
+    219,
+    223,
+    225,
+    234,
+    236,
+    240,
+    245,
+    246,
+    249,
+    251,
+    253,
+    257,
+    268,
+    269,
+    271,
+    275,
+]
+GENDER = 'male'
